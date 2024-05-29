e255e704820ce733072a2cdfe8cf0c52390888a9
PicToWeb <picronantoine34@gmail.com>
2024-05-29 22:09:45 +0200

    exo4 - Code Propre

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -3,17 +3,31 @@ package ex4;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/** Class who proposes any methods to transform date on String 
+ * 
+ */
 public class DateUtils {
 
+	/** DEFAULT_FORMAT */
+	private static final String DEFAULT_FORMAT = "dd/MM/yyyy HH:mm:ss";
+
+	/** Transform a date on String on keeping display format
+	 * @param pattern display format
+	 * @param date date to format
+	 * @return
+	 */
 	public static String format(String pattern, Date date) {
-		
+
 		SimpleDateFormat formateur = new SimpleDateFormat(pattern);
 		return formateur.format(date);
 	}
-	
+
+	/** Transform a date on String using default display format
+	 * @param date date to format
+	 * @return
+	 */
 	public static String formatDefaut(Date date) {
-		
-		SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		return formateur.format(date);
+
+		return format(DEFAULT_FORMAT, date);
 	}
 }
